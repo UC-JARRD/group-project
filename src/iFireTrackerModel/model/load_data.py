275224492fee79ec02408de8caf073f2a7b586c0
jess-napper <napper.jess@gmail.com>
2024-05-21 18:36:36 +1200

    Updated load_data to read in a csv file for the given day. This closes Make model read in data only from today #26

--- src/iFireTrackerModel/model/load_data.py
+++ src/iFireTrackerModel/model/load_data.py
@@ -1,10 +1,12 @@
 import logging
 import pandas as pd
+import datetime
+import os
 
 LOG_PATH = './logs/main_execution.log'
 INPUT_WEATHER_STATIONS_PATH = './data/input/canterbury_weather_stations.csv'
-INPUT_FIRE_RISK_PATH = './data/input/fire_risk.csv'
 MERGED_LAND_USE_STATIONS_PATH = './data/input/merged/land_use_areas_per_station.csv'
+INPUT_FIRE_RISK_PATH_PREFIX = './data/input/'
 
 # Configure logging
 logger = logging.getLogger(__name__)
@@ -13,20 +15,40 @@ handler = logging.FileHandler(LOG_PATH)
 handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(handler)
 
+def get_today_fire_risk_path():
+    # Get today's date
+    today = datetime.datetime.today().strftime('%Y-%m-%d')
+    # Construct the file name
+    fire_risk_filename = f"{today}_fire_risk.csv"
+    # Construct the full file path
+    fire_risk_filepath = os.path.join(INPUT_FIRE_RISK_PATH_PREFIX, fire_risk_filename)
+    return fire_risk_filepath
 
 def load_data():
     logging.info("[load_data.py] Loading weather station data...")
+
     # Load weather station data
     try:
         weather_stations_coords = pd.read_csv(INPUT_WEATHER_STATIONS_PATH) # coordinates of each station
-        fire_risk = pd.read_csv(INPUT_FIRE_RISK_PATH) # fire risk at each station (will come from database eventually)
         logging.info("[load_data.py] Weather station data loaded successfully.")
     except FileNotFoundError:
         logging.error("[load_data.py] Failed to load weather station data. File not found.")
         raise
 
-    logging.info("[load_data.py] Merging weather station data...")
+    # Get today's fire risk file path
+    fire_risk_filepath = get_today_fire_risk_path()
+    
+    try:
+        # Load fire risk data
+        fire_risk = pd.read_csv(fire_risk_filepath)
+        logging.info(f"[load_data.py] Fire risk data for today ({fire_risk_filepath}) loaded successfully.")
+    except FileNotFoundError:
+        logging.error(f"[load_data.py] Failed to load fire risk data for today ({fire_risk_filepath}). File not found.")
+        raise
+
+
     # Merge the datasets on the 'station_name' column
+    logging.info("[load_data.py] Merging weather station data...")
     try:
         station_fire_risk = pd.merge(weather_stations_coords, fire_risk, on='station_name')
         logging.info("[load_data.py] Datasets merged successfully.")
